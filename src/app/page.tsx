8158455e531faaae0dafff72073b037c8ac56c16
levintoo <tooklevin@gmail.com>
2025-05-07 14:33:32 +0300

    feat(ui): add shadcn UI and button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
+      <div>hello</div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
